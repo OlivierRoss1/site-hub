4b44b0c5a88a507f5d801d515dc06d8eaae6f00a
Olivier Ross <olivier.ross@usherbrooke.ca>
2019-02-13 23:33:55 -0500

    initial

---
+++ donnees/references.js
@@ -0,0 +1,18 @@
+var references = [
+  {
+    titre: "Surveillance capitalism",
+    description: "Une oeuvre distopique",
+    date: "01/12/2018",
+    tags: ["non-fiction", "interet"]
+  },
+  {
+    titre: "Les misérables",
+    description: "À lire",
+    date: "01/01/2019"
+  },
+  {
+    titre: "Le rouge et le noir",
+    description: "Aucune idée",
+    date: "02/02/2019"
+  }
+]
