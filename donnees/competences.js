ce482894b7ca3afd67a9c323b2d4233c75f0e474
Olivier Ross <olivier.ross@usherbrooke.ca>
2019-03-10 20:26:01 -0400

    Donnees hub ajoutees

---
+++ donnees/competences.js
@@ -0,0 +1,32 @@
+var competences = {
+  "name": "HUB",
+  "children": [{
+    "name": "Organisation",
+    "children": [{
+      "name": "",
+      "children": [{
+        "name": "AgglomerativeCluster"
+      }]
+    }]
+  }, {
+    "name": "Communication",
+    "children": [{
+      "name": "cluster",
+      "children": [{
+        "name": "AgglomerativeCluster"
+      }]
+    }]
+  },
+  {
+    "name": "Création",
+    "children": [{
+        "name": "Fabrication numérique",
+        "children": [
+          {"name": "Fabrication additive"},
+          {"name": "Fabrication soustractive"}
+        ]
+      },
+      {"name": "Expérience muséale numérique"}
+    ]
+  }]
+}
